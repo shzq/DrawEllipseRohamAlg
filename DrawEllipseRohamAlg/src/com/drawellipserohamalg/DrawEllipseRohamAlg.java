18869185a0dad0164132db1b0927b9e70f4c1531
Sahli <sahli.haziq@gmail.com>
2013-11-03 23:19:34 -0800

    Trying to draw the ellipse

---
+++ DrawEllipseRohamAlg/src/com/drawellipserohamalg/DrawEllipseRohamAlg.java
@@ -0,0 +1,199 @@
+package com.drawellipserohamalg;
+
+import java.awt.BasicStroke;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Toolkit;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+
+public class DrawEllipseRohamAlg extends JApplet {
+	
+	// Hard coded focus
+	private static Focus a = new Focus(0,1);
+	private static Focus b = new Focus(2,0);
+	private static Focus c = new Focus(3,5);
+	private static Focus d = new Focus(5,5);
+	private static Focus e = new Focus(5,7);
+	
+	// Add foci into one array
+	private static List<Focus> foci = new ArrayList<Focus>();
+	
+	private final static double C = 25;
+	private final static double STEPSIZE = 0.05;
+	private final static double THRESHOLD = 0.1;
+	
+	private static List<Double> px = new ArrayList<Double>();
+	private static List<Double> py = new ArrayList<Double>();
+	private static double ax;
+	private static double ay;
+	
+	private static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	private static int windowWidth = screenSize.width/2;
+	private static int windowHeight = screenSize.height - 40;
+	private static Point2D origin;
+	
+	private final int xScale = 10;
+	private final int yScale = 10;
+	
+	/** The formula to change coordinate to screen coordinate
+	 * is: 
+	 * xOnScreenCoord = yourXValue*yourXScale + origin.getX()
+	 * yOnScreenCoord = -yourYValue*yourYScale + origin.getY() **/
+	
+	// Return foci as Line2D in screen coord
+	private List<Line2D> getFociAsLine() {
+		List<Focus> fociInScreenCoord = new ArrayList<Focus>();
+		List<Line2D> fociAsLine = new ArrayList<Line2D>();
+
+		for (int i = 0; i < foci.size(); i++) {
+			// Don't forget the -ve on y
+			fociInScreenCoord.add(new Focus(foci.get(i).getPoint().getX()*xScale + origin.getX(),
+					-foci.get(i).getPoint().getY()*yScale + origin.getY()) );
+		}
+		
+	     for (int i = 0; i < fociInScreenCoord.size(); i++) {
+	    	 fociAsLine.add(new Line2D.Double(fociInScreenCoord.get(i).getPoint(),fociInScreenCoord.get(i).getPoint()));
+	     }
+	     
+	     return fociAsLine;
+	}
+	
+	// Return the coordinate of px py in screen coordinate in a list
+	private List<Point2D> getPointpxpy() {
+		List<Point2D> pxpy = new ArrayList<Point2D>();
+		if (px.size() != py.size()) {
+			System.out.println("ERROR: px size and py size does not match. Program exiting");
+			System.exit(ERROR);
+		} else {
+			for (int i = 0; i < px.size(); i++)
+				pxpy.add(new Point2D.Double(px.get(i)*xScale + origin.getX(), -py.get(i)*yScale + origin.getY()));
+		}
+		return pxpy;
+	}
+	
+	public void paint(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		final BasicStroke stroke = new BasicStroke(3.0f);
+		final BasicStroke fwStroke = new BasicStroke(2.0f);
+		
+	    g2.draw(new Line2D.Double(origin.getX(), 0, origin.getX(), windowHeight));
+	    g2.draw(new Line2D.Double(0, origin.getY(), windowWidth, origin.getY()));
+	     
+	     
+	    // draw foci. Set it as a line by giving the same point twice in screen position
+	    List<Line2D> fociAsLine = getFociAsLine();
+	    g2.setStroke(stroke);
+	    for (int i = 0; i < fociAsLine.size(); i++) {
+	    	g2.draw(fociAsLine.get(i));
+	    }
+	    
+	    // draw point pxpy in screen coordinate
+	    g2.setStroke(fwStroke);
+	    Point2D axay = new Point2D.Double(ax*xScale + origin.getX(),-ay*yScale + origin.getY());
+	    Line2D pointaxay = new Line2D.Double(axay,axay);
+	    g2.draw(pointaxay);
+	    
+	    // draw ellipse. pxpy is in screen coordinate
+	    g2.setStroke(stroke);
+	    List<Point2D> pxpy = getPointpxpy();
+	  /*  // Turn it to a connected line
+	    List<Line2D> linepxpy = new ArrayList<Line2D>();
+	    for (int i = 0; i < pxpy.size(); i++) {
+	    	if (i == pxpy.size()-1)
+	    		linepxpy.add(new Line2D.Double(pxpy.get(i), pxpy.get(0)));
+	    	else
+	    		linepxpy.add(new Line2D.Double(pxpy.get(i), pxpy.get(i+1)));
+	    }
+	 */   
+	    // Turn it to a Line2D
+	    List<Line2D> linepxpy = new ArrayList<Line2D>();
+	    for (int i = 0; i < pxpy.size(); i++) {
+	    		linepxpy.add(new Line2D.Double(pxpy.get(i), pxpy.get(i)));
+	    }
+	    
+	    for (int i = 0; i < linepxpy.size(); i++) {
+	    	g2.draw(linepxpy.get(i));
+	    	if (i == linepxpy.size() - 1)
+	    		System.out.println("Finish drawing ellipse line");
+	    }
+	    
+	}
+	
+	public static void main(String[] args) {
+		
+		JFrame f = new JFrame("DrawEllipseRohamAlg");
+		f.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) { System.exit(0); }
+		});
+		JApplet applet = new DrawEllipseRohamAlg();
+		f.getContentPane().add("Center", applet);
+		applet.init();
+		f.pack();
+		f.setSize(new Dimension(windowWidth,windowHeight));
+		f.setVisible(true);
+		
+		origin = new Point2D.Double(windowWidth/2, windowHeight/2);
+		
+		foci.add(a);
+		foci.add(b);  foci.add(c); foci.add(d); foci.add(e);
+		
+		for (int i = 0; i < foci.size(); i++) {
+			ax += foci.get(i).getPoint().getX();
+		}
+		ax = ax/foci.size();
+		
+		for (int i = 0; i < foci.size(); i++) {
+			ay += foci.get(i).getPoint().getY();
+		}
+		ay = ay/foci.size();
+		
+		for (double i = ax; i <= C; i += STEPSIZE) {
+			for (double j = ay; j <= C; j += STEPSIZE) {
+				Calculate.sumOfDistance(foci, i, j);
+			}
+		}
+		
+		for (double i = -C; i <= ax; i += STEPSIZE) {
+			for (double j = ay; j <= C; j += STEPSIZE) {
+				Calculate.sumOfDistance(foci, i, j);
+			}
+		}
+		
+		for (double i = ax; i <= C; i += STEPSIZE) {
+			for (double j = -C; j <= ay; j += STEPSIZE) {
+				Calculate.sumOfDistance(foci, i, j);
+			}
+		}
+		
+		for (double i = -C; i <= ax; i += STEPSIZE) {
+			for (double j = -C; j <= ay; j += STEPSIZE) {
+				Calculate.sumOfDistance(foci, i, j);
+			}
+		}
+	}
+	
+	private static class Calculate {
+		
+		private static void sumOfDistance(List<Focus> foci, double i, double j) {
+			double d = 0;	
+			for (int k = 0; k < foci.size(); k++) {
+					d = Math.sqrt( Math.pow(foci.get(k).getPoint().getX() - i, 2) + Math.pow(foci.get(k).getPoint().getY(), 2) );
+				}
+			d -= C;
+			if (Math.abs(d) < THRESHOLD) {
+				px.add(i);
+				py.add(j);
+			}
+		}
+	}
+	
+}
--- DrawEllipseRohamAlg/src/com/drawellipserohamalg/Focus.java
+++ DrawEllipseRohamAlg/src/com/drawellipserohamalg/Focus.java
@@ -1,38 +1,39 @@
 package com.drawellipserohamalg;
 
 import java.awt.Point;
+import java.awt.geom.Point2D;
 
 public class Focus {
 	
 	// For this example we use Point. Later will use LatLong.
-	private final Point point;
+	private final Point2D point;
 	private final double weight;
 	
 	// Constructor
-	public Focus(int x, int y, double weight) {
-		this.point = new Point(x,y);
+	public Focus(double x, double y, double weight) {
+		this.point = new Point2D.Double(x,y);
 		this.weight = weight;
 	}
 	
 	// Constructor
-	public Focus(int x, int y) {
-		this.point = new Point(x,y);
+	public Focus(double x, double y) {
+		this.point = new Point2D.Double(x,y);
 		this.weight = 1;
 	}
 	
 	// Constructor
-	public Focus(Point f, double weight) {
+	public Focus(Point2D f, double weight) {
 		this.point = f;
 		this.weight = weight;
 	}
 	
 	// Constructor
-	public Focus(Point f) {
+	public Focus(Point2D f) {
 		this.point = f;
 		this.weight = 1;
 	}
 	
-	public Point getPoint() {
+	public Point2D getPoint() {
 		return this.point;
 	}
 	
